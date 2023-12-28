d08b1197b364fd989ea40549bbb18ae6dfebc8b3
Sealington <150572349+Sealington@users.noreply.github.com>
2023-12-28 11:35:37 -0500

    Create basicify.js
    basicify

---
+++ basicify.js
@@ -0,0 +1,1 @@
+var script = document.createElement('script'); script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; document.getElementsByTagName('head')[0].appendChild(script); $(document).ready(function() { $("style").remove(); $("link").remove(); console.info ("%c Basicified by @Sealington :3 ", "background: #222; color: #1cb1d6;"); });
